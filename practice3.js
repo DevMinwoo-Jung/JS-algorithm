0e7cbdaa6f1a9a3d1f5aedd0742bb8f7a2d5cac5
Minwoo <applejung312@gmail.com>
2022-03-16 23:04:35 +0900

    20220316

--- practice3.js
+++ practice3.js
@@ -48,52 +48,79 @@
   // // console.log(solution(")()(")); // false
   // // console.log(solution("())()(()")); // false
 
-  function example(input) {
-    let frequentNum = [...input];
-    let checkNum = {};
+  // function example(input) {
+  //   let frequentNum = [...input];
+  //   let checkNum = {};
+  //   let answer = [];
+  //   let answer2 = [];
+  //   let checkMaxNum = 0;
+
+  //   Object.defineProperties(Array.prototype, {
+  //     count: {
+  //       value: function (value) {
+  //         return this.filter((x) => x == value).length;
+  //       },
+  //     },
+  //   });
+
+  //   let key = [...new Set(frequentNum)];
+
+  //   for (let i = 0; i < 3; i++) {
+  //     console.log(`${key[i]}'s count === ${frequentNum.count(key[i])}`);
+  //     checkNum[`${key[i]}`] = frequentNum.count(key[i]);
+  //   }
+  //   console.log(checkNum);
+
+  //   answer = Object.keys(checkNum)
+  //     .map((num) => checkNum[num])
+  //     .sort((a, b) => b - a);
+  //   console.log(
+  //     "이건 최대 많이 나온 숫자가 몇 번 등장했는지 알려주는 것" + answer
+  //   );
+  //   // 이건 최대 많이 나온 숫자가 몇 번 등장했는지 알려주는 것 그래서 여기서 가장 첫번쨰 꺼만 빼옴
+
+  //   checkMaxNum = checkMaxNum = Math.max(
+  //     [
+  //       ...Object.keys(checkNum)
+  //         .map((num) => checkNum[num])
+  //         .sort((a, b) => b - a),
+  //     ][0]
+  //   );
+  //   console.log(frequentNum);
+  //   console.log(checkMaxNum);
+  //   answer2 = Object.keys(checkNum).filter(
+  //     (num) => checkNum[num] === checkMaxNum
+  //   );
+  //   console.log(answer2);
+  //   // 가장 많이 등장한 숫자
+  // }
+
+  // example([1, 2, 2, 2, 4, 4, 4]);
+
+  function solution(input) {
+    let inputTonumber = input.split("");
     let answer = [];
-    let answer2 = [];
-    let checkMaxNum = 0;
 
-    Object.defineProperties(Array.prototype, {
-      count: {
-        value: function (value) {
-          return this.filter((x) => x == value).length;
-        },
-      },
-    });
-
-    let key = [...new Set(frequentNum)];
-
-    for (let i = 0; i < 3; i++) {
-      console.log(`${key[i]}'s count === ${frequentNum.count(key[i])}`);
-      checkNum[`${key[i]}`] = frequentNum.count(key[i]);
+    for (let i = 0; i < inputTonumber.length; i++) {
+      if (inputTonumber[i] === "I") {
+        answer.push(3);
+      } else if (inputTonumber[i] === "V") {
+        answer.push(5);
+      } else if (inputTonumber[i] === "X") {
+        answer.push(10);
+      } else if (inputTonumber[i] === "L") {
+        answer.push(50);
+      } else if (inputTonumber[i] === "C") {
+        answer.push(100);
+      } else if (inputTonumber[i] === "D") {
+        answer.push(500);
+      } else if (inputTonumber[i] === "M") {
+        answer.push(1000);
+      }
     }
-    console.log(checkNum);
+    answer = answer.reduce((a, b) => a + b);
 
-    answer = Object.keys(checkNum)
-      .map((num) => checkNum[num])
-      .sort((a, b) => b - a);
-    console.log(
-      "이건 최대 많이 나온 숫자가 몇 번 등장했는지 알려주는 것" + answer
-    );
-    // 이건 최대 많이 나온 숫자가 몇 번 등장했는지 알려주는 것 그래서 여기서 가장 첫번쨰 꺼만 빼옴
-
-    checkMaxNum = checkMaxNum = Math.max(
-      [
-        ...Object.keys(checkNum)
-          .map((num) => checkNum[num])
-          .sort((a, b) => b - a),
-      ][0]
-    );
-    console.log(frequentNum);
-    console.log(checkMaxNum);
-    answer2 = Object.keys(checkNum).filter(
-      (num) => checkNum[num] === checkMaxNum
-    );
-    console.log(answer2);
-    // 가장 많이 등장한 숫자
+    console.log(answer);
   }
-
-  example([1, 2, 2, 2, 4, 4, 4]);
+  solution("III");
 }
