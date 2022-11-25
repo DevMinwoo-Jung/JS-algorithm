4ad5aac339277093e54d327e1d4c5b72020dbe08
DevMinwooJung <applejung312@gmail.com>
2022-11-25 09:37:07 +0900

    friday

--- file4.js
+++ file4.js
@@ -3,14 +3,18 @@
 // [2, 1, 1, 2, 3, 1, 2, 3, 1]	2
 // [1, 3, 2, 1, 2, 1, 3, 1, 2]	0
 //   //
-  function solution(ingredient) {
-    const answer = [];
-    const obj = {}
-    ingredient.map((element, index) => element.includes(answer[index]) ? obj[index]++ : obj[index] = 1);
-    console.log(obj);
-    return answer;
-  }
-  solution([2, 1, 1, 2, 3, 1, 2, 3, 1]);
+  // function solution(ingredient) {
+  //   const answer = [];
+  //   const obj = {}
+  //   ingredient.map((element, index) => element.includes(answer[index]) ? obj[index]++ : obj[index] = 1);
+  //   console.log(obj);
+  //   return answer;
+  // }
+  // solution([2, 1, 1, 2, 3, 1, 2, 3, 1]);
+
+ // 인기있는 아이스크림
+ // -- 코드를 입력하세요
+ // SELECT FLAVOR from FIRST_HALF ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID
 }
 
 /// ㅣength를 하나씩 더하면 될거 같은데...
